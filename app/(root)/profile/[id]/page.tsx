5651bd7c9c781ac6b5fe7480b7a5110fd0da59c5
3kingwang <103560098+3kingwang@users.noreply.github.com>
2024-01-08 17:40:11 +0800

    search

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,9 +1,11 @@
 import ProfileHeader from "@/components/shared/ProfileHeader"
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Tabs, TabsList, TabsTrigger,TabsContent } from "@/components/ui/tabs"
 import { profileTabs } from "@/constants"
 import { fetchUser } from "@/lib/actions/user.actions"
 import { currentUser } from "@clerk/nextjs"
 import { redirect } from "next/navigation"
+import Image from "next/image"
+import ThreadsTab from "@/components/shared/ThreadsTab"
 const Page = async ({ params }: { params: { id: string } }) => {
   const user = await currentUser()
   if (!user) return null
@@ -24,11 +26,35 @@ const Page = async ({ params }: { params: { id: string } }) => {
           <TabsList className="tab">
             {profileTabs.map((tab) => (
               <TabsTrigger key={tab.label} value={tab.value}>
-                <Image src={tab.icon} alt={tab.label} width={24} height={24} />
+                <Image
+                  src={tab.icon}
+                  alt={tab.label}
+                  width={24}
+                  height={24}
+                  className="object-contain"
+                />
                 <p className="max-sm:hidden">{tab.label}</p>
+                {tab.label === "Threads" && (
+                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
+                    {userInfo.threads?.length}
+                  </p>
+                )}
               </TabsTrigger>
             ))}
           </TabsList>
+          {profileTabs.map((tab) => (
+            <TabsContent
+              key={`content-${tab.label}`}
+              value={tab.value}
+              className="w-full text-light-1"
+            >
+              <ThreadsTab
+                currentUserId={user.id}
+                accountId={userInfo.id}
+                accountType="User"
+              />
+            </TabsContent>
+          ))}
         </Tabs>
       </div>
     </section>
